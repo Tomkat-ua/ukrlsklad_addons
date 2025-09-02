228beb395173478e7a0302dec780fab67a051d94
Kostevych Viacheslav <tomkat.s@gmail.com>
2025-09-02 17:04:50 +0300

    refactor 101

--- ghist_.py
+++ ghist_.py
@@ -22,13 +22,7 @@ def index():
         print('search_str',search_str)
         con = db.get_connection()
         cur = con.cursor()
-        cur.execute(""" select distinct ts.num as serial_id, ts.tovar_id,ts.tovar_ser_num,tn.kod,tn.name as tov_name
-                        ,case when tss.num is not null  then 'Втрачено' else 'На обліку' end as status
-                        from tovar_serials ts
-                        inner join tovar_name tn on tn.num = ts.tovar_id
-                        left join tovar_serials tss on tss.tovar_ser_num = ts.tovar_ser_num and tss.doc_type_id =11
-                        where ts.tovar_ser_num like ?
-                        and ts.doc_type_id in( 8,9) """,[search_str.strip()])
+        cur.execute(" select * from usadd_web.ghist_list (?) ",[search_str.strip()])
         rows = cur.fetchall()
         columns = [desc[0] for desc in cur.description]
         df = pd.DataFrame(rows, columns=columns)
@@ -45,17 +39,7 @@ def datails(search_str):
     con = db.get_connection()
     cur = con.cursor()
     ##### movies #########
-    cur.execute(""" SELECT vd.doc_type,dt.name AS doc_name,vd.num,vd.nu,vd.date_dok,sn.num as  sklad_id,sn.name AS sklad_name,
-                           case  when ts.tovar_ser_kolvo  >0 then '+' else '-' end as oper_type                       
-                    FROM   tovar_name tn
-                      inner join tovar_serials ts on ts.tovar_id = tn.num
-                        and ts.tovar_ser_num = (select t.tovar_ser_num from tovar_serials t where t.num = ?)  
-                        and ts.ser_type_id = 0  and ts.doc_type_id <> 8
-                      inner JOIN doc_types dt ON (ts.doc_type_id = dt.num)
-                      inner JOIN sklad_names sn ON (ts.sklad_id = sn.num)
-                      inner join view_alldocs vd on vd.doc_type =  ts.doc_type_id
-                       and vd.num= ts.doc_id   AND vd.firma_id = 170 and vd.is_move = 1   
-                    ORDER BY VD.DATE_DOK ,vd.num ,ts.tovar_ser_kolvo  """, [search_str])
+    cur.execute("select * from usadd_web.ghist_det_movies (?)", [search_str])
     rows = cur.fetchall()
     columns = [desc[0] for desc in cur.description]
     df = pd.DataFrame(rows, columns=columns)
@@ -64,8 +48,8 @@ def datails(search_str):
     cur.close()
 
     ##### details ########
-    cur.execute( 'select * from usa_v_ghist_details where serial_id = ?',[search_str])
-
+    # cur.execute( 'select * from usa_v_ghist_details where serial_id = ?',[search_str])
+    cur.execute('select * from usadd_web.ghist_det_header (?)', [search_str])
     rows_det = cur.fetchall()
     columns_det = [desc[0] for desc in cur.description]
     df_det = pd.DataFrame(rows_det, columns=columns_det)
