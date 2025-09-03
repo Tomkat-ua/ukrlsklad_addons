d1474eb49158b47f5cb405118af040a390b9e814
Kostevych Viacheslav <tomkat.s@gmail.com>
2025-09-03 10:01:13 +0300

    refactor 102

--- ghist_.py
+++ ghist_.py
@@ -5,13 +5,24 @@ import pandas as pd
 
 title = 'Інформація по номеру'
 
+
 def fetch_named(cursor):
     """Генерує записи як словники з іменами полів."""
     columns = [desc[0].lower() for desc in cursor.description]  # або без .lower()
     for row in cursor:
         yield dict(zip(columns, row))
 
-
+def data_for_header(serial,serial_id = None):
+    con = db.get_connection()
+    cur = con.cursor()
+    cur.execute(" select * from usadd_web.ghist_list (?,?) ",[serial.strip(),serial_id])
+    rows = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    df = pd.DataFrame(rows, columns=columns)
+    df_display = df.fillna('')
+    data = df_display.to_dict(orient='records')
+    con.close()
+    return data
 
 
 def index():
@@ -20,15 +31,17 @@ def index():
         # search_str = request.form.get('tov_serial')
         search_str = request.form['tov_serial']
         print('search_str',search_str)
-        con = db.get_connection()
-        cur = con.cursor()
-        cur.execute(" select * from usadd_web.ghist_list (?) ",[search_str.strip()])
-        rows = cur.fetchall()
-        columns = [desc[0] for desc in cur.description]
-        df = pd.DataFrame(rows, columns=columns)
-        df_display = df.fillna('')
-        data = df_display.to_dict(orient='records')
-        con.close()
+        # con = db.get_connection()
+        # cur = con.cursor()
+        # cur.execute(" select * from usadd_web.ghist_list (?) ",[search_str.strip()])
+        # rows = cur.fetchall()
+        # columns = [desc[0] for desc in cur.description]
+        # df = pd.DataFrame(rows, columns=columns)
+        # df_display = df.fillna('')
+        # data = df_display.to_dict(orient='records')
+        # con.close()
+        global data
+        data = data_for_header(search_str)
         return render_template('ghist_.html', title=title,rows = data,search_value=search_str.strip())
     return render_template('ghist_.html',title=title)
 
@@ -38,6 +51,7 @@ def datails(search_str):
     search_str = int(search_str)
     con = db.get_connection()
     cur = con.cursor()
+
     ##### movies #########
     cur.execute("select * from usadd_web.ghist_det_movies (?)", [search_str])
     rows = cur.fetchall()
@@ -48,14 +62,15 @@ def datails(search_str):
     cur.close()
 
     ##### details ########
-    # cur.execute( 'select * from usa_v_ghist_details where serial_id = ?',[search_str])
-    cur.execute('select * from usadd_web.ghist_det_header (?)', [search_str])
-    rows_det = cur.fetchall()
-    columns_det = [desc[0] for desc in cur.description]
-    df_det = pd.DataFrame(rows_det, columns=columns_det)
-    df_display_det = df_det.fillna('')
-    data_datail = df_display_det.to_dict(orient='records')
+    # cur.execute('select * from usadd_web.ghist_det_header (?)', [search_str])
+    # rows_det = cur.fetchall()
+    # columns_det = [desc[0] for desc in cur.description]
+    # df_det = pd.DataFrame(rows_det, columns=columns_det)
+    # df_display_det = df_det.fillna('')
+    # data_datail = df_display_det.to_dict(orient='records')
+    data = data_for_header('0',search_str)
+    print(data[0])
     con.close()
-    return render_template('ghist_det.html',title=title,rows = data_movies,row=data_datail[0] )
+    return render_template('ghist_det.html',title=title,rows = data_movies,row=data[0] )
     # print(f"details {row_id}")
 
--- serials.py
+++ serials.py
@@ -6,13 +6,24 @@ def serials_search():
     result = []
     search_term = ''
     if request.method == 'POST':
-        sql = "select * from monitoring.get_serials_by_tov_sklad(?)"
-        tov_id = request.form.get('name', '').strip()
-        result=db.get_data(sql,[tov_id])
+        sql = "select * from usadd_web.get_serials_by_tov_sklad(?,?)"
+        tov_id = request.form.get('search_tovar', '').strip()
+        sklad_id = request.form.get('search_sklad', '').strip() or None
+        print(tov_id,sklad_id)
+        result=db.get_data(sql,[tov_id,sklad_id])
         total = len(result)
         sql = "select tn.name from tovar_name tn where tn.num = ?"
         tov_name = db.get_data(sql, [tov_id],1)
-        return render_template('serials.html', result=result, title = 'Пошук номерів',total=total,tov_name=str(tov_name[0]))
+        if sklad_id == None:
+            sklad_id = 300000001
+        sql = "select sn.name from sklad_names sn where sn.num = ?"
+        sklad_name = db.get_data(sql, [sklad_id], 1)
+
+        sql = """select sn.num as id, sn.name from sklad_names sn
+                    where sn.num >=? order by 2"""
+        sklads = db.get_data(sql,[300000000])
+
+        return render_template('serials.html',sklads=sklads, result=result, title = 'Пошук номерів',total=total,sklad_name=sklad_name[0],tov_name=str(tov_name[0]))
     return render_template('serials.html', result=result, title='Пошук номерів', total=None, tov_name=None)
 
 
