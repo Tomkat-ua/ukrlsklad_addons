db6e69971c88d6c83ccb46b1124e2b54ecd6753b
Kostevych Viacheslav <tomkat.s@gmail.com>
2025-09-03 17:45:16 +0300

    refactor 105

--- ghist_.py
+++ ghist_.py
@@ -2,9 +2,7 @@ from flask import  request,render_template
 import db
 import pandas as pd
 
-
 title = 'Інформація по номеру'
-
 
 def fetch_named(cursor):
     """Генерує записи як словники з іменами полів."""
@@ -12,10 +10,19 @@ def fetch_named(cursor):
     for row in cursor:
         yield dict(zip(columns, row))
 
-def data_for_header(serial,serial_id = None):
+def data_for_module(param,module):
+    match module:
+        case 'list':
+            sql = "select * from usadd_web.ghist_list (?) rows 1000"
+        case 'header':
+            sql = "select * from usadd_web.GHIST_DET_HEADER (?)"
+        case 'movies':
+            sql = "select * from usadd_web.ghist_det_movies (?)"
+        case _:  # optional default case
+            sql = '*'
     con = db.get_connection()
     cur = con.cursor()
-    cur.execute(" select * from usadd_web.ghist_list (?,?) ",[serial.strip(),serial_id])
+    cur.execute(sql, [param])
     rows = cur.fetchall()
     columns = [desc[0] for desc in cur.description]
     df = pd.DataFrame(rows, columns=columns)
@@ -24,24 +31,14 @@ def data_for_header(serial,serial_id = None):
     con.close()
     return data
 
-
 def index():
     ######### LIST #######################
     if request.method == "POST":
         # search_str = request.form.get('tov_serial')
         search_str = request.form['tov_serial']
         print('search_str',search_str)
-        # con = db.get_connection()
-        # cur = con.cursor()
-        # cur.execute(" select * from usadd_web.ghist_list (?) ",[search_str.strip()])
-        # rows = cur.fetchall()
-        # columns = [desc[0] for desc in cur.description]
-        # df = pd.DataFrame(rows, columns=columns)
-        # df_display = df.fillna('')
-        # data = df_display.to_dict(orient='records')
-        # con.close()
-        global data
-        data = data_for_header(search_str)
+        # data = data_for_list(search_str)
+        data = data_for_module(search_str,'list')
         return render_template('ghist_.html', title=title,rows = data,search_value=search_str.strip())
     return render_template('ghist_.html',title=title)
 
@@ -49,28 +46,13 @@ def index():
 def datails(search_str):
     print('search_str',search_str)
     search_str = int(search_str)
-    con = db.get_connection()
-    cur = con.cursor()
 
-    ##### movies #########
-    cur.execute("select * from usadd_web.ghist_det_movies (?)", [search_str])
-    rows = cur.fetchall()
-    columns = [desc[0] for desc in cur.description]
-    df = pd.DataFrame(rows, columns=columns)
-    df_display = df.fillna('')
-    data_movies = df_display.to_dict(orient='records')
-    cur.close()
-
-    ##### details ########
-    # cur.execute('select * from usadd_web.ghist_det_header (?)', [search_str])
-    # rows_det = cur.fetchall()
-    # columns_det = [desc[0] for desc in cur.description]
-    # df_det = pd.DataFrame(rows_det, columns=columns_det)
-    # df_display_det = df_det.fillna('')
-    # data_datail = df_display_det.to_dict(orient='records')
-    data = data_for_header('0',search_str)
-    print(data[0])
-    con.close()
-    return render_template('ghist_det.html',title=title,rows = data_movies,row=data[0] )
+##### movies #########
+    data_movies = data_for_module(search_str,'movies')
+##### details ########
+    data_header = data_for_module(search_str,'header')
+    # print(data_header[0])
+    # con.close()
+    return render_template('ghist_det.html',title=title,rows = data_movies,row=data_header[0] )
     # print(f"details {row_id}")
 
