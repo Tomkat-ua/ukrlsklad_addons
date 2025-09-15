dc6829bc4c104bc9043f2749df19e1b781e3c11e
Kostevych Viacheslav <tomkat.s@gmail.com>
2025-09-15 14:29:56 +0300

    serials add all

--- serials.py
+++ serials.py
@@ -13,18 +13,18 @@ def serials_search():
         sql = "select * from usadd_web.get_serials_by_tov_sklad(?,?)"
         tov_id = request.form.get('search_tovar', '').strip()
         sklad_id = request.form.get('sklad_id', '').strip() or None
+        if sklad_id == "None":
+            sklad_id = None
         print(tov_id,sklad_id)
         result=db.get_data(sql,[tov_id,sklad_id])
         total = len(result)
         sql = "select tn.name from tovar_name tn where tn.num = ?"
         tov_name = db.get_data(sql, [tov_id],1)
-        if sklad_id == None:
-            sklad_id = 300000001
         sql = "select sn.name from sklad_names sn where sn.num = ?"
         sklad_name = db.get_data(sql, [sklad_id], 1)
 
-        return render_template('serials.html', result=result, sklads=sklads,search_tovar=tov_id
-                               ,title = 'Пошук номерів',total=total,sklad_name=sklad_name[0]
+        return render_template('serials.html', result=result if result else '', sklads=sklads,search_tovar=tov_id
+                               ,title = 'Пошук номерів',total=total,sklad_name=sklad_name[0] if sklad_name else ''
                                ,tov_name=str(tov_name[0] if tov_name else '' ))
 
     return render_template('serials.html', sklads=sklads,result=result, title='Пошук номерів', total=None, tov_name=None)
