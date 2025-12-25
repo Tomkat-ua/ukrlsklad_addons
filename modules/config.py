b54360f3b38f1cff858c6f41266394c29a6d945e
Kostevych Viacheslav <tomkat.s@gmail.com>
2025-12-25 16:14:47 +0200

    add versioner 2

--- modules/config.py
+++ modules/config.py
@@ -6,7 +6,6 @@ load_dotenv()
 server_port = 3001
 api_key =  os.getenv('API_KEY')
 env = os.getenv('ENV','DEV')
-app_version = os.getenv('APP_VERSION')
 db_server = os.getenv("DB_HOST")
 db_port = int(os.getenv("DB_PORT"))
 db_path = os.getenv("DB_PATH")
