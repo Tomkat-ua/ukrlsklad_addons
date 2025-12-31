03260b449461a848fc84d19246ccb4c14c3e386c
Kostevych Viacheslav <tomkat.s@gmail.com>
2025-12-31 15:37:13 +0200

    ref  serials 12

--- modules/serials.py
+++ modules/serials.py
@@ -52,6 +52,8 @@ def serials_check():
         cur = conn.cursor()
         total = 0
         total_err = 0
+        total_sap = 0
+        total_acc = 0
         for sn in serial_list:
             try:
                 # Виклик процедури (залежно від того, як вона написана у вас)
@@ -70,13 +72,22 @@ def serials_check():
                 """, [sn])
 
                 row = cur.fetchone()
-                status = f"{row[0]}|{row[1]}|{row[3]}|{row[4]} " \
-                    if row else "Не знайдено"
+                if row:
+                    status = f"{row[0]}|{row[1]}|{row[3]}|{row[4]} "
+                    # if row else "Не знайдено" total_err = total_err + 1
+                    state_raw = row[4]
+                    state = state_raw.strip()
+                    if state == 'На обліку':
+                        total_acc = total_acc + 1
+                    if state == 'Списано (акт пуску)':
+                        total_sap = total_sap + 1
+                else:
+                    status = "Не знайдено"
+                    total_err = total_err + 1
 
                 results.append({'sn': sn, 'status': status})
                 total = total + 1
-                if status == 'Не знайдено':
-                    total_err = total_err + 1
+
             except Exception as e:
                 results.append({'sn': sn, 'status': f"Помилка: {str(e)}"})
         conn.close()
@@ -84,43 +95,56 @@ def serials_check():
         return f"Помилка підключення до БД: {str(e)}"
     formatted_serials = ", ".join([f"'{s}'" for s in serial_list])
 
-    sql_g = f""" select  ts.tovar_id,tn.kod ,tn.name,count(*) as count_
-                    from tovar_serials ts
-                     inner join tovar_name tn on tn.num = ts.tovar_id
-                    where (ts.doc_type_id = 8 or ts.doc_type_id = 9 )
-                    and ts.tovar_ser_num in ({formatted_serials})
-                    group by 1 ,2 ,3  order by 3"""
+    sql_g = f"""
+                select * from (
+                 select ts.tovar_id   ,tn.kod   ,tn.name
+                 ,usadd_web.serial_status_code(ts.tovar_ser_num) as status_code
+                 ,count(*) as count_
+                from tovar_serials ts
+                 inner join tovar_name tn on tn.num = ts.tovar_id
+                where (ts.doc_type_id = 8 or ts.doc_type_id = 9 )
+                and ts.tovar_ser_num in  ({formatted_serials} )
+                group by 1 ,2 ,3,4) where status_code = 0
+                order by 3
+"""
     if not serial_list:
         data_g = []  # або повернути порожній DataFrame
     else:
         data_g = db.data_module(sql_g,'')
         session['data_g'] = data_g
-    return render_template('serial_check.html', results=results,raw_data=raw_text,
-                           total=total,total_err=total_err,data_g=data_g)
+        session['formatted_serials'] = formatted_serials
+    print('total_sap',total_sap)
+    return render_template('serial_check.html', results=results,
+                           raw_data=raw_text,
+                           total=total,
+                           total_err=total_err,
+                           data_g=data_g,
+                           total_sap=total_sap,
+                           total_acc=total_acc)
 
 
 def add_to_actv():
     # Дістаємо дані з сесії
     data_to_process = session.get('data_g', [])
+    serials_to_process = session.get('formatted_serials', [])
 
+    req_data = request.get_json()
+    doc_id = req_data.get('doc_id')
+    print(doc_id)
     if not data_to_process:
         return "Помилка: Немає даних для обробки. Спочатку запустіть перевірку."
-
     processed_count = 0
     con = db.get_connection()
     cur = con.cursor()
     for row in data_to_process:
-        # Припустимо, ваша процедура приймає TOVAR_ID
         tovar_id = row.get('TOVAR_ID')
-        req_data = request.get_json()
-        doc_id   = req_data.get('doc_id')
+        # req_data = request.get_json()
+        # doc_id   = req_data.get('doc_id')
         count_   = row.get('COUNT_')
         try:
             cur.callproc('import.i_actvr_det',[doc_id,tovar_id,count_])
             processed_count += 1
         except Exception as e:
-            # Повертаємо ПОВНИЙ текст помилки як рядок у JSON
-            # error_full_text = str(e)
             error_full_text = str(e.args[0]) if hasattr(e, 'args') and e.args else str(e)
             return jsonify({
                 "status": "error",
@@ -128,8 +152,47 @@ def add_to_actv():
             }), 500
     con.commit()
     con.close()
-    # flash(f"Успішно передано {processed_count} записів",'info')
-    # return redirect(url_for('serial_scheck'))
+    ## add serials
+    serials_to_actv(serials_to_process,doc_id)
+    ##
     return {"status": "ok", "message": f"Оброблено {processed_count} записів"}
-    # return f"Успішно оброблено {processed_count} записів у БД!"
 
+def serials_to_actv(serials,doc_id):
+    sql = f""" execute block
+as
+    declare variable tovar_id tnum ;
+    declare variable serial   tsm_kod;
+    declare variable doc_id   tnum ;
+    declare variable sklad_id tnum;
+begin
+ select a.sklad_id from actvr a where a.num = {doc_id} into sklad_id;
+  if ( sklad_id is null) then
+  begin
+   exception ex_common_error using ('[ERROR-00100: SKLAD_ID is NULL]');
+   exit;
+  end
+ for
+    select
+     ts.tovar_id,ts.tovar_ser_num
+    from tovar_serials ts
+    where (ts.doc_type_id = 8 or ts.doc_type_id = 9 )
+    and usadd_web.serial_status_code(ts.tovar_ser_num)  = 0
+    and ts.tovar_ser_num in({serials})
+     into tovar_id, serial
+    do
+        execute procedure import.i_serial(:tovar_id,:serial,{doc_id},:sklad_id,-1);
+end """
+    print(sql)
+    try:
+        con = db.get_connection()
+        cur = con.cursor()
+        cur.execute(sql,'')
+        con.commit()
+        con.close()
+    except Exception as e:
+        error_full_text = str(e.args[0]) if hasattr(e, 'args') and e.args else str(e)
+        return jsonify({
+            "status": "error",
+            "message": error_full_text
+        }), 500
+    return {"status": "ok", "message": f"OK"}
