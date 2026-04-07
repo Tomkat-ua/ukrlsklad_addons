722dae868c81a94f827e0b87a0c4ddd906656f20
Kostevych Viacheslav <tomkat.s@gmail.com>
2026-04-07 11:45:13 +0300

    add serial gen

---
+++ modules/serial_gen.py
@@ -0,0 +1,39 @@
+from flask import render_template,request
+
+from datetime import datetime
+def gen_date():
+    year = datetime.now().strftime("%y")
+    match datetime.now().month:
+        case 10:
+            month = "A"
+        case 11:
+            month = "B"
+        case 12:
+            month = "C"
+        case _:
+            month = datetime.now().month
+    return f"{year}{month}"
+
+
+def generator():
+    # prefix = ''
+    # serial_len=0
+    # crow = 0
+    result = []
+    if request.method == 'POST':
+        prefix = request.form.get('pref','').strip()
+        serial_len = int(request.form.get('len','').strip())
+        crow = int(request.form.get('crow', '0').strip() or 0)
+        prefix_len = len(prefix)
+        for n in range(1,crow + 1):
+            tail = str(n)
+            if prefix_len + len(tail)+len(gen_date()) < serial_len:
+                tail = tail.rjust(serial_len - prefix_len - len(gen_date()),"0")
+            result.append(prefix+gen_date() +tail)
+    return render_template('serials_gen.html'
+                           ,title = 'Генерація серійників'
+                           ,data = result)
+
+
+
+
